631d30db48c75b8d83dded43c27751d299f4be98
Morgan-Ngetich <ngetichmorgan6@gmail.com>
2023-12-11 01:52:59 +0300

    Added some comments

--- consonant_values.py
+++ consonant_values.py
@@ -1,17 +1,30 @@
+# Define a function named 'solve' that takes a string as input
 def solve(string):
-  vowels = 'aeiou' # Defined a string containing all vowels
-  max_value = 0
-  current_value = 0
-  
-  # Loop through each character in the for loop
-  for char in string:
-    if char not in vowels:
-      current_value += ord(char) - ord('a') + 1
-      if current_value > max_value:
-        max_value = current_value
-    else:
-      current_value = 0
-      
-  return max_value
+    # Define a string containing all vowels
+    vowels = 'aeiou'
+    # Initialize variables to track the maximum and current values of consonant substrings
+    max_value = 0
+    current_value = 0
 
-print(solve('zodiacs'))
+    # Loop through each character in the input string
+    for char in string:
+        # Check if the character is not a vowel
+        if char not in vowels:
+            # Calculate the value of the consonant using ASCII values
+            current_value += ord(char) - ord('a') + 1
+            # Update the maximum value if the current consonant value is greater
+            if current_value > max_value:
+                max_value = current_value
+        else:
+            # Reset the current_value to 0 if the character is a vowel
+            current_value = 0
+
+    # Return the maximum value of consonant substrings found in the string
+    return max_value
+
+# Prompt the user to enter a lowercase string with no spaces
+user_input = input("Enter a lowercase string with no spaces: ")
+# Call the solve function with the user-provided input and store the result
+result = solve(user_input)
+# Display the highest value of consonant substrings in the input string
+print(f"The highest value of consonant substrings is: {result}")
